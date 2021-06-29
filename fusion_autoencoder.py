187376bab4bb8a037f5404f84257be08a8992c0b
akashsara <akashsara@outlook.com>
2021-06-29 15:58:01 +0530

    add fusion autoencoder

---
+++ fusion_autoencoder.py
@@ -0,0 +1,396 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import pytorch_msssim
+from tqdm import tqdm
+
+import utils.data as data
+import utils.graphics as graphics
+import utils.loss as loss
+import models
+
+seed = 42
+np.random.seed(seed)
+_ = torch.manual_seed(seed)
+
+################################################################################
+#################################### Config ####################################
+################################################################################
+learning_rate = 1e-4
+epochs = 2
+batch_size = 64
+num_dataloader_workers = 0
+
+experiment_name = f"fusion_dual_input_autoencoder_v1"
+
+num_layers = 4
+max_filters = 512
+image_size = 64
+latent_dim = 2048
+use_noise_images = True
+small_conv = True  # To use the 1x1 convolution layer
+use_sum = False  # Use a sum instead of a mean for our loss function
+use_ssim_loss = False
+mse_weight = 1
+ssim_weight = 1
+num_mse = 0  # Each increment halves the image sizes and takes the MSE
+
+data_prefix = "data\\final\\standard"
+fusion_data_prefix = "data\\final\\fusions"
+output_prefix = f"data\\{experiment_name}"
+
+train_data_folder = os.path.join(data_prefix, "train")
+val_data_folder = os.path.join(data_prefix, "val")
+test_data_folder = os.path.join(data_prefix, "test")
+
+fusion_train_data_folder = os.path.join(fusion_data_prefix, "train")
+fusion_val_data_folder = os.path.join(fusion_data_prefix, "val")
+fusion_test_data_folder = os.path.join(fusion_data_prefix, "test")
+
+output_dir = os.path.join(output_prefix, "generated")
+loss_output_path = output_prefix
+model_output_path = os.path.join(output_prefix, "model.pt")
+
+animation_output_path = os.path.join(output_prefix, "animation.mp4")
+animation_sample_image_name = os.path.join(output_prefix, "animation_base.jpg")
+fusion_animation_output_path = os.path.join(output_prefix, "fusion_animation.mp4")
+fusion_animation_sample_image_name = os.path.join(
+    output_prefix, "fusion_animation_base.jpg"
+)
+
+test_sample_input_name = os.path.join(output_prefix, "test_sample_input.jpg")
+test_sample_output_name = os.path.join(output_prefix, "test_sample_output.jpg")
+fusion_test_sample_input_name = os.path.join(
+    output_prefix, "fusion_test_sample_input.jpg"
+)
+fusion_test_sample_output_name = os.path.join(
+    output_prefix, "fusion_test_sample_output.jpg"
+)
+################################################################################
+##################################### Setup ####################################
+################################################################################
+
+# Setup Device
+gpu = torch.cuda.is_available()
+device = torch.device("cuda" if gpu else "cpu")
+print(gpu, device)
+
+# Create Output Paths
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+################################################################################
+################################## Data Setup ##################################
+################################################################################
+
+# Preprocess & Create Data Loaders
+transform = data.image2tensor_resize(image_size)
+
+train_data = data.FusionDatasetV2(
+    train_data_folder,
+    fusion_train_data_folder,
+    data_prefix,
+    transform,
+    use_noise_images,
+)
+val_data = data.FusionDatasetV2(
+    val_data_folder, fusion_val_data_folder, data_prefix, transform, use_noise_images
+)
+test_data = data.FusionDatasetV2(
+    test_data_folder, fusion_test_data_folder, data_prefix, transform, use_noise_images
+)
+
+train_dataloader = torch.utils.data.DataLoader(
+    train_data,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_dataloader_workers,
+    pin_memory=gpu,
+)
+val_dataloader = torch.utils.data.DataLoader(
+    val_data,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_dataloader_workers,
+    pin_memory=gpu,
+)
+test_dataloader = torch.utils.data.DataLoader(
+    test_data,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_dataloader_workers,
+    pin_memory=gpu,
+)
+
+# Creating a sample set to visualize the model's training
+sample = data.get_samples_from_FusionDatasetV2(val_data, 16, "standard")
+fusion_sample = data.get_samples_from_FusionDatasetV2(val_data, 4, "fusion")
+sample = sample.to(device)
+sample_base = fusion_sample[:, 0].to(device)
+sample_fusee = fusion_sample[:, 1].to(device)
+sample_fusion = fusion_sample[:, 2].to(device)
+
+################################################################################
+##################################### Model ####################################
+################################################################################
+
+# Create Model
+model = models.FusionAE(
+    max_filters=max_filters,
+    num_layers=num_layers,
+    input_image_dimensions=image_size,
+    latent_dim=latent_dim,
+    small_conv=small_conv,
+)
+model.to(device)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+ssim_module = None
+if use_ssim_loss:
+    ssim_module = pytorch_msssim.SSIM(
+        data_range=1.0, win_size=11, win_sigma=1.5, K=(0.01, 0.03)
+    )
+
+################################################################################
+################################### Training ###################################
+################################################################################
+
+# Train
+all_samples = []
+all_fusion_samples = []
+all_train_loss = []
+all_val_loss = []
+
+# Get an initial "epoch 0" sample
+with torch.no_grad():
+    epoch_sample = model(sample, sample)
+    epoch_base = model(sample_base, sample_base)
+    epoch_fusee = model(sample_fusee, sample_fusee)
+    epoch_fusion = model(sample_base, sample_fusee)
+    epoch_fusion_sample = torch.stack(
+        (epoch_base, epoch_fusee, epoch_fusion), dim=1
+    ).flatten(end_dim=1)
+
+# Add sample reconstruction to our list
+all_samples.append(epoch_sample.detach().cpu())
+all_fusion_samples.append(epoch_fusion_sample.detach().cpu())
+
+for epoch in range(epochs):
+    train_loss = 0
+    val_loss = 0
+
+    # Training Loop
+    model.train()
+    for iteration, batch in enumerate(tqdm(train_dataloader)):
+        # Reset gradients back to zero for this iteration
+        optimizer.zero_grad()
+        # Move batch to device
+        _, (base, fusee, fusion) = batch  # (names), (images)
+        base = base.to(device)
+        fusee = fusee.to(device)
+        fusion = fusion.to(device)
+        # Run our model & get outputs
+        reconstructed = model(base, fusee)
+        # Calculate reconstruction loss
+        batch_loss, loss_dict = loss.mse_ssim_loss(
+            reconstructed,
+            fusion,
+            use_sum=False,
+            ssim_module=ssim_module,
+            mse_weight=mse_weight,
+            ssim_weight=ssim_weight,
+        )
+        # For multiple MSE
+        # For every MSE, we halve the image size
+        # And take the MSE between the resulting images
+        for i in range(num_mse):
+            new_size = image_size // pow(2, i + 1)
+            with torch.no_grad():
+                resized_batch = nn.functional.interpolate(
+                    fusion, size=new_size, mode="bilinear"
+                )
+            resized_output = nn.functional.interpolate(
+                reconstructed, size=new_size, mode="bilinear"
+            )
+            mse = loss.mse_loss(resized_output, resized_batch, use_sum)
+            batch_loss += mse
+            loss_dict["MSE"] += mse.item()
+        # Backprop
+        batch_loss.backward()
+        # Update our optimizer parameters
+        optimizer.step()
+        # Add the batch's loss to the total loss for the epoch
+        train_loss += loss_dict["MSE"] + loss_dict["SSIM"]
+        break
+
+    # Validation Loop
+    model.eval()
+    with torch.no_grad():
+        for iteration, batch in enumerate(tqdm(val_dataloader)):
+            # Move batch to device
+            _, (base, fusee, fusion) = batch  # (names), (images)
+            base = base.to(device)
+            fusee = fusee.to(device)
+            fusion = fusion.to(device)
+            # Run our model & get outputs
+            reconstructed = model(base, fusee)
+            # Calculate reconstruction loss
+            batch_loss, loss_dict = loss.mse_ssim_loss(
+                reconstructed,
+                fusion,
+                use_sum=False,
+                ssim_module=ssim_module,
+                mse_weight=mse_weight,
+                ssim_weight=ssim_weight,
+            )
+            # For multiple MSE
+            # For every MSE, we halve the image size
+            # And take the MSE between the resulting images
+            for i in range(num_mse):
+                new_size = image_size // pow(2, i + 1)
+                with torch.no_grad():
+                    resized_batch = nn.functional.interpolate(
+                        fusion, size=new_size, mode="bilinear"
+                    )
+                resized_output = nn.functional.interpolate(
+                    reconstructed, size=new_size, mode="bilinear"
+                )
+                mse = loss.mse_loss(resized_output, resized_batch, use_sum)
+                batch_loss += mse
+                loss_dict["MSE"] += mse.item()
+            # Add the batch's loss to the total loss for the epoch
+            val_loss += loss_dict["MSE"] + loss_dict["SSIM"]
+            break
+
+        # Get reconstruction of our sample
+        epoch_sample = model(sample, sample)
+        epoch_base = model(sample_base, sample_base)
+        epoch_fusee = model(sample_fusee, sample_fusee)
+        epoch_fusion = model(sample_base, sample_fusee)
+        epoch_fusion_sample = torch.stack(
+            (epoch_base, epoch_fusee, epoch_fusion), dim=1
+        ).flatten(end_dim=1)
+
+    # Add sample reconstruction to our list
+    all_samples.append(epoch_sample.detach().cpu())
+    all_fusion_samples.append(epoch_fusion_sample.detach().cpu())
+
+    # Compute the average losses for this epoch
+    train_loss = train_loss / len(train_dataloader)
+    all_train_loss.append(train_loss)
+
+    val_loss = val_loss / len(val_dataloader)
+    all_val_loss.append(val_loss)
+
+    # Print Metrics
+    print(
+        f"\nEpoch: {epoch+1}/{epochs}:\
+        \nTrain Loss = {train_loss}\
+        \nVal Loss = {val_loss}"
+    )
+
+################################################################################
+################################## Save & Test #################################
+################################################################################
+# Generate Loss Graph
+graphics.draw_loss(all_train_loss, all_val_loss, loss_output_path, mode="autoencoder")
+
+# Save Model
+torch.save(model.state_dict(), model_output_path)
+
+# Plot Animation Sample
+fig, axis = graphics.make_grid(("Sample", sample.detach().cpu()), 4, 4)
+plt.savefig(animation_sample_image_name)
+# Plot Fusion Animation Sample
+fusion_sample = [x for y in fusion_sample.detach().cpu() for x in y]
+fig, axis = graphics.make_grid(("Sample", fusion_sample), 4, 3)
+plt.savefig(fusion_animation_sample_image_name)
+
+# Create & Save Animation
+anim = graphics.make_animation(graphics.make_grid, all_samples)
+anim.save(animation_output_path)
+# Create & Save Fusion Animation
+fusion_anim = graphics.make_animation(graphics.make_grid, all_fusion_samples, width=3)
+fusion_anim.save(fusion_animation_output_path)
+
+model.eval()
+
+# Evaluate on Test Images
+# Save Generated Images & Calculate Metrics
+# Testing Loop - Standard
+all_mse = []
+all_ssim = []
+with torch.no_grad():
+    for iteration, batch in enumerate(tqdm(test_dataloader)):
+        # Move batch to device
+        filenames, images = batch  # (names), (images)
+        _, _, fusion_filenames = filenames
+        base, fusee, fusion = images
+        base = base.to(device)
+        fusee = fusee.to(device)
+        fusion = fusion.to(device)
+
+        # Run our model & get outputs
+        reconstructed = model(base, fusee)
+
+        # Calculate Metrics
+        mse = nn.functional.mse_loss(reconstructed, fusion)
+        ssim_score = pytorch_msssim.ssim(
+            reconstructed,
+            fusion,
+            data_range=1.0,
+            win_size=11,
+            win_sigma=1.5,
+            K=(0.01, 0.03),
+        )
+
+        # Add metrics to tracking list
+        all_mse.append(mse.detach().cpu().numpy())
+        all_ssim.append(ssim_score.detach().cpu().numpy())
+
+        # Save
+        reconstructed = reconstructed.permute(0, 2, 3, 1).detach().cpu().numpy()
+        for image, filename in zip(reconstructed, fusion_filenames):
+            plt.imsave(os.path.join(output_dir, filename), image)
+        break
+
+# Print Metrics
+mse = np.asarray(all_mse).mean()
+ssim_score = np.asarray(all_ssim).mean()
+print(f"\nMSE = {mse}, SSIM = {ssim_score}")
+
+# Pick a couple of sample images for an Input v Output comparison
+test_sample = data.get_samples_from_FusionDatasetV2(test_data, 16, "standard")
+fusion_test_sample = data.get_samples_from_FusionDatasetV2(test_data, 4, "fusion")
+
+# Plot A Set of Test Images
+fig, axis = graphics.make_grid(("Test Sample", test_sample), 4, 4)
+plt.savefig(test_sample_input_name)
+to_plot = [x for y in fusion_test_sample for x in y]
+fig, axis = graphics.make_grid(("Fusion Test Sample", to_plot), 4, 3)
+plt.savefig(fusion_test_sample_input_name)
+
+# Get Outputs for selected images
+with torch.no_grad():
+    test_sample = test_sample.to(device)
+    test_sample_base = fusion_test_sample[:, 0].to(device)
+    test_sample_fusee = fusion_test_sample[:, 1].to(device)
+    # Normal Image
+    output_samples = model(test_sample, test_sample).detach().cpu()
+    # Fusion Image
+    output_base = model(test_sample_base, test_sample_base).detach().cpu()
+    output_fusee = model(test_sample_fusee, test_sample_fusee).detach().cpu()
+    output_fusion = model(test_sample_base, test_sample_fusee).detach().cpu()
+    output_fusions = torch.stack(
+        (output_base, output_fusee, output_fusion), dim=1
+    ).flatten(end_dim=1)
+
+# Plot A Set of Reconstructed Test Images
+fig, axis = graphics.make_grid(("Test Sample", output_samples), 4, 4)
+plt.savefig(test_sample_output_name)
+fig, axis = graphics.make_grid(("Fusion Test Sample", output_fusions), 4, 3)
+plt.savefig(fusion_test_sample_output_name)
--- models.py
+++ models.py
@@ -307,6 +307,7 @@ class ConvolutionalVAE(nn.Module):
         log_var = self.fc_log_var(hidden_state)
         return mu, log_var
 
+
 class FusionVAE(nn.Module):
     def __init__(
         self,
@@ -466,6 +467,152 @@ class FusionVAE(nn.Module):
         mu = self.fc_mu(hidden_state)
         log_var = self.fc_log_var(hidden_state)
         return mu, log_var
+
+
+class FusionAE(nn.Module):
+    def __init__(
+        self,
+        image_channels=3,
+        max_filters=512,
+        num_layers=4,
+        kernel_size=2,
+        stride=2,
+        padding=0,
+        latent_dim=128,
+        input_image_dimensions=96,
+        small_conv=False,
+    ):
+        super(FusionAE, self).__init__()
+        if small_conv:
+            num_layers += 1
+        channel_sizes = self.calculate_channel_sizes(
+            image_channels, max_filters, num_layers
+        )
+
+        # Encoder
+        encoder_layers = nn.ModuleList()
+        # Encoder Convolutions
+        for i, (in_channels, out_channels) in enumerate(channel_sizes):
+            if small_conv and i == 0:
+                # 1x1 Convolution
+                encoder_layers.append(
+                    nn.Conv2d(
+                        in_channels=in_channels,
+                        out_channels=out_channels,
+                        kernel_size=1,
+                        stride=1,
+                        padding=0,
+                    )
+                )
+            else:
+                # Convolutional Layer
+                encoder_layers.append(
+                    nn.Conv2d(
+                        in_channels=in_channels,
+                        out_channels=out_channels,
+                        kernel_size=kernel_size,
+                        stride=stride,
+                        padding=padding,
+                        bias=False,
+                    )
+                )
+            # Batch Norm
+            encoder_layers.append(nn.BatchNorm2d(out_channels))
+            # ReLU
+            encoder_layers.append(nn.ReLU())
+        # Flatten Encoder Output
+        encoder_layers.append(nn.Flatten())
+        # Calculate shape of the flattened image
+        hidden_dim, image_size = self.get_flattened_size(
+            input_image_dimensions, encoder_layers
+        )
+
+        # Hidden Dim -> Latent Dim
+        encoder_layers.append(nn.Linear(hidden_dim, latent_dim))
+        encoder_layers.append(nn.Sigmoid())
+        self.encoder = nn.Sequential(*encoder_layers)
+
+        # Decoder
+        decoder_layers = nn.ModuleList()
+        # Latent Dim -> Hidden Dim
+        decoder_layers.append(nn.Linear(latent_dim, hidden_dim))
+        decoder_layers.append(nn.Sigmoid())
+        # Unflatten to a shape of (Channels, Height, Width)
+        decoder_layers.append(nn.Unflatten(1, (max_filters, image_size, image_size)))
+        # Decoder Convolutions
+        for i, (out_channels, in_channels) in enumerate(channel_sizes[::-1]):
+            if small_conv and i == num_layers - 1:
+                # 1x1 Transposed Convolution
+                decoder_layers.append(
+                    nn.ConvTranspose2d(
+                        in_channels=in_channels,
+                        out_channels=out_channels,
+                        kernel_size=1,
+                        stride=1,
+                        padding=0,
+                    )
+                )
+            else:
+                # Add Transposed Convolutional Layer
+                decoder_layers.append(
+                    nn.ConvTranspose2d(
+                        in_channels=in_channels,
+                        out_channels=out_channels,
+                        kernel_size=kernel_size,
+                        stride=stride,
+                        padding=padding,
+                        bias=False,
+                    )
+                )
+            # Batch Norm
+            decoder_layers.append(nn.BatchNorm2d(out_channels))
+            # ReLU if not final layer
+            if i != num_layers - 1:
+                decoder_layers.append(nn.ReLU())
+            # Sigmoid if final layer
+            else:
+                decoder_layers.append(nn.Sigmoid())
+        self.decoder = nn.Sequential(*decoder_layers)
+
+    def calculate_layer_size(self, input_size, kernel_size, stride, padding=0):
+        numerator = input_size - kernel_size + (2 * padding)
+        denominator = stride
+        return (numerator // denominator) + 1
+
+    def get_flattened_size(self, image_size, encoder_layers):
+        for layer in encoder_layers:
+            if "Conv2d" in str(layer):
+                kernel_size = layer.kernel_size[0]
+                stride = layer.stride[0]
+                padding = layer.padding[0]
+                filters = layer.out_channels
+                image_size = self.calculate_layer_size(
+                    image_size, kernel_size, stride, padding
+                )
+        return filters * image_size * image_size, image_size
+
+    def calculate_channel_sizes(self, image_channels, max_filters, num_layers):
+        channel_sizes = [(image_channels, max_filters // np.power(2, num_layers - 1))]
+        for i in range(1, num_layers):
+            prev = channel_sizes[-1][-1]
+            new = prev * 2
+            channel_sizes.append((prev, new))
+        return channel_sizes
+
+    def forward(self, x1, x2, return_logits=False):
+        # Encode
+        hidden_state1 = self.encoder(x1)
+        hidden_state2 = self.encoder(x2)
+        hidden_state = (0.4 * hidden_state1) + (0.4 * hidden_state2)
+        # Decode
+        if return_logits:
+            for layer in self.decoder[:-1]:
+                hidden_state = layer(hidden_state)
+            logits = hidden_state
+            reconstructed = self.decoder[-1](hidden_state)
+            return reconstructed, logits
+        else:
+            return self.decoder(hidden_state)
 
 
 class VAEGANEncoder(nn.Module):
