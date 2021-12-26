e4b1bc7d84c2a0f50cc39dda328f8350c07c730d
Akash Saravanan <akashsara@outlook.com>
2021-12-25 21:01:33 -0700

    add vae prior

--- models/vae.py
+++ models/vae.py
@@ -320,3 +320,166 @@ class FusionVAE(nn.Module):
         mu = self.fc_mu(hidden_state)
         log_var = self.fc_log_var(hidden_state)
         return mu, log_var
+
+
+class VAEPrior(nn.Module):
+    def __init__(
+        self,
+        max_filters=512,
+        num_layers=4,
+        kernel_size=2,
+        stride=2,
+        padding=0,
+        latent_dim=128,
+        input_dimensions=64,
+        small_conv=False,
+        num_classes=256,
+    ):
+        super(VAEPrior, self).__init__()
+        if small_conv:
+            num_layers += 1
+        channel_sizes = self.calculate_channel_sizes(
+            num_classes, max_filters, num_layers
+        )
+        self.out_channels = num_classes
+        self.out_size = input_dimensions
+
+        # Embedding Layer
+        self.embedding = nn.Embedding(num_embeddings=num_classes, embedding_dim=num_classes)
+
+        # Encoder
+        encoder_layers = nn.ModuleList()
+        # Encoder Convolutions
+        for i, (in_channels, out_channels) in enumerate(channel_sizes):
+            if small_conv and i == 0:
+                # 1x1 Convolution
+                encoder_layers.append(
+                    nn.Conv2d(
+                        in_channels=in_channels,
+                        out_channels=out_channels,
+                        kernel_size=1,
+                        stride=1,
+                        padding=0,
+                    )
+                )
+            else:
+                # Convolutional Layer
+                encoder_layers.append(
+                    nn.Conv2d(
+                        in_channels=in_channels,
+                        out_channels=out_channels,
+                        kernel_size=kernel_size,
+                        stride=stride,
+                        padding=padding,
+                        bias=False,
+                    )
+                )
+            # Batch Norm
+            encoder_layers.append(nn.BatchNorm2d(out_channels))
+            # ReLU
+            encoder_layers.append(nn.ReLU())
+        # Flatten Encoder Output
+        encoder_layers.append(nn.Flatten())
+        self.encoder = nn.Sequential(*encoder_layers)
+
+        # Calculate shape of the flattened image
+        hidden_dim, image_size = self.get_flattened_size(input_dimensions)
+
+        # Latent Space
+        self.fc_mu = nn.Linear(hidden_dim, latent_dim)
+        self.fc_log_var = nn.Linear(hidden_dim, latent_dim)
+
+        # Decoder
+        decoder_layers = nn.ModuleList()
+        # Feedforward/Dense Layer to expand our latent dimensions
+        decoder_layers.append(nn.Linear(latent_dim, hidden_dim))
+        # Unflatten to a shape of (Channels, Height, Width)
+        decoder_layers.append(nn.Unflatten(1, (max_filters, image_size, image_size)))
+        # Decoder Convolutions
+        for i, (out_channels, in_channels) in enumerate(channel_sizes[::-1]):
+            if small_conv and i == num_layers - 1:
+                # 1x1 Transposed Convolution
+                decoder_layers.append(
+                    nn.ConvTranspose2d(
+                        in_channels=in_channels,
+                        out_channels=out_channels,
+                        kernel_size=1,
+                        stride=1,
+                        padding=0,
+                    )
+                )
+            else:
+                # Add Transposed Convolutional Layer
+                decoder_layers.append(
+                    nn.ConvTranspose2d(
+                        in_channels=in_channels,
+                        out_channels=out_channels,
+                        kernel_size=kernel_size,
+                        stride=stride,
+                        padding=padding,
+                        bias=False,
+                    )
+                )
+            # Batch Norm
+            decoder_layers.append(nn.BatchNorm2d(out_channels))
+            decoder_layers.append(nn.ReLU())
+        self.decoder = nn.Sequential(*decoder_layers)
+
+    def calculate_layer_size(self, input_size, kernel_size, stride, padding=0):
+        numerator = input_size - kernel_size + (2 * padding)
+        denominator = stride
+        return (numerator // denominator) + 1
+
+    def get_flattened_size(self, image_size):
+        for layer in self.encoder:
+            if "Conv2d" in str(layer):
+                kernel_size = layer.kernel_size[0]
+                stride = layer.stride[0]
+                padding = layer.padding[0]
+                filters = layer.out_channels
+                image_size = self.calculate_layer_size(
+                    image_size, kernel_size, stride, padding
+                )
+        return filters * image_size * image_size, image_size
+
+    def calculate_channel_sizes(self, image_channels, max_filters, num_layers):
+        channel_sizes = [(image_channels, max_filters // np.power(2, num_layers - 1))]
+        for i in range(1, num_layers):
+            prev = channel_sizes[-1][-1]
+            new = prev * 2
+            channel_sizes.append((prev, new))
+        return channel_sizes
+
+    def forward(self, x, return_logits=False):
+        x = self.embedding(x.flatten(start_dim=1))
+        x = x.reshape(-1, self.out_channels, self.out_size, self.out_size)
+        mu, log_var = self.get_latent_variables(x)
+        # Reparameterize
+        z = self.reparameterize(mu, log_var)
+        # Decode
+        if return_logits:
+            for layer in self.decoder[:-1]:
+                z = layer(z)
+            logits = z
+            reconstructed = self.decoder[-1](z)
+        else:
+            logits = None
+            reconstructed = self.decoder(z)
+        reconstructed = reconstructed.view(
+            -1, self.out_channels, self.out_size, self.out_size
+        )
+        return reconstructed, mu, log_var, logits
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(log_var.mul(0.5))  # log sqrt(x) = log x^0.5 = 0.5 log x
+        epsilon = torch.randn_like(mu)
+        z = mu + (epsilon * std)
+        return z
+
+    def get_latent_variables(self, x):
+        # Encode
+        hidden_state = self.encoder(x)
+        # Get latent variables
+        mu = self.fc_mu(hidden_state)
+        log_var = self.fc_log_var(hidden_state)
+        return mu, log_var
---
+++ vae_prior.py
@@ -0,0 +1,374 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import pytorch_msssim
+from tqdm import tqdm
+
+import utils.data as data
+import utils.graphics as graphics
+import utils.loss as loss
+from models import vae, vqvae
+
+seed = 42
+np.random.seed(seed)
+_ = torch.manual_seed(seed)
+
+################################################################################
+#################################### Config ####################################
+################################################################################
+learning_rate = 1e-4
+epochs = 25
+batch_size = 32
+num_dataloader_workers = 0
+
+experiment_name = f"convolutional_vae_v10"
+
+# VQ-VAE Config
+mode = "discrete"
+vq_vae_image_size = 64
+vq_vae_experiment_name = f"vq_vae_v5.5"
+vq_vae_num_layers = 2
+vq_vae_max_filters = 512
+vq_vae_use_max_filters = True
+vq_vae_num_embeddings = 256
+vq_vae_embedding_dim = 64
+vq_vae_commitment_cost = 0.25
+vq_vae_small_conv = True  # To use the 1x1 convolution layer
+
+# VAE Prior Config
+image_size = vq_vae_image_size // (2 ** vq_vae_num_layers)
+num_layers = 2
+max_filters = 512
+input_dim = 1
+latent_dim = 256
+num_classes = vq_vae_num_embeddings
+use_noise_images = True
+small_conv = False  # To use the 1x1 convolution layer
+kl_d_weight = 1  # equivalent to beta in a Beta-VAE
+
+# Data Config
+data_prefix = "data\\pokemon\\final\\standard"
+output_prefix = f"data\\{experiment_name}"
+vq_vae_model_prefix = f"outputs\\{vq_vae_experiment_name}"
+vq_vae_model_path = os.path.join(vq_vae_model_prefix, "model.pt")
+
+train_data_folder = os.path.join(data_prefix, "train")
+val_data_folder = os.path.join(data_prefix, "val")
+test_data_folder = os.path.join(data_prefix, "test")
+
+output_dir = os.path.join(output_prefix, "generated")
+loss_output_path = output_prefix
+model_output_path = os.path.join(output_prefix, "model.pt")
+
+animation_output_path = os.path.join(output_prefix, "animation.mp4")
+animation_sample_image_name = os.path.join(output_prefix, "animation_base.jpg")
+
+test_sample_input_name = os.path.join(output_prefix, "test_sample_input.jpg")
+test_sample_output_name = os.path.join(output_prefix, "test_sample_output.jpg")
+################################################################################
+##################################### Setup ####################################
+################################################################################
+
+# Setup Device
+gpu = torch.cuda.is_available()
+device = torch.device("cuda" if gpu else "cpu")
+print(gpu, device)
+
+# Create Output Paths
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+################################################################################
+################################## Data Setup ##################################
+################################################################################
+
+# Load Data
+train = data.load_images_from_folder(train_data_folder, use_noise_images)
+val = data.load_images_from_folder(val_data_folder, use_noise_images)
+test = data.load_images_from_folder(test_data_folder, use_noise_images)
+
+# Preprocess & Create Data Loaders
+transform = data.image2tensor_resize(vq_vae_image_size)
+
+train_data = data.CustomDataset(train, transform)
+val_data = data.CustomDataset(val, transform)
+test_data = data.CustomDataset(test, transform)
+
+train_dataloader = torch.utils.data.DataLoader(
+    train_data,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_dataloader_workers,
+    pin_memory=gpu,
+)
+val_dataloader = torch.utils.data.DataLoader(
+    val_data,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_dataloader_workers,
+    pin_memory=gpu,
+)
+test_dataloader = torch.utils.data.DataLoader(
+    test_data,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_dataloader_workers,
+    pin_memory=gpu,
+)
+
+# Creating a sample set to visualize the model's training
+sample = data.get_samples_from_data(val_data, 16)
+
+################################################################################
+##################################### Model ####################################
+################################################################################
+
+# Create & Load VQVAE Model
+vq_vae = vqvae.VQVAE(
+    num_layers=vq_vae_num_layers,
+    input_image_dimensions=vq_vae_image_size,
+    small_conv=vq_vae_small_conv,
+    embedding_dim=vq_vae_embedding_dim,
+    num_embeddings=vq_vae_num_embeddings,
+    commitment_cost=vq_vae_commitment_cost,
+    use_max_filters=vq_vae_use_max_filters,
+    max_filters=vq_vae_max_filters,
+)
+vq_vae.load_state_dict(torch.load(vq_vae_model_path, map_location=device))
+vq_vae.eval()
+vq_vae.to(device)
+
+# Create Model
+model = vae.VAEPrior(
+    input_dimensions=image_size,
+    num_classes=num_classes,
+    num_layers=num_layers,
+    max_filters=max_filters,
+    latent_dim=latent_dim,
+    small_conv=small_conv,
+)
+model.to(device)
+print(model)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+criterion = nn.CrossEntropyLoss()
+################################################################################
+################################### Training ###################################
+################################################################################
+
+# Train
+all_samples = []
+all_train_loss = []
+all_val_loss = []
+
+# Get an initial "epoch 0" sample
+model.eval()
+target_shape = (sample.shape[0], image_size, image_size, vq_vae_embedding_dim)
+with torch.no_grad():
+    _, _, _, encodings = vq_vae(sample.to(device))
+    epoch_sample = encodings.reshape(-1, image_size, image_size)
+    epoch_sample, _, _, _ = model(epoch_sample)
+    epoch_sample = epoch_sample.argmax(dim=1, keepdim=True)
+    epoch_sample = epoch_sample.flatten(start_dim=1).view(-1, 1)
+    epoch_sample = vq_vae.quantize_and_decode(epoch_sample, target_shape, device)
+
+# Add sample reconstruction to our list
+all_samples.append(epoch_sample.detach().cpu())
+
+for epoch in range(epochs):
+    train_loss = 0
+    train_recon_loss = 0
+    train_kl_d = 0
+    val_loss = 0
+    val_recon_loss = 0
+    val_kl_d = 0
+
+    # Training Loop
+    model.train()
+    for iteration, batch in enumerate(tqdm(train_dataloader)):
+        # Reset gradients back to zero for this iteration
+        optimizer.zero_grad()
+
+        # Move batch to device
+        _, batch = batch  # Returns key, value for each Pokemon
+        batch = batch.to(device)
+
+        with torch.no_grad():
+            # Get Encodings from vq_vae
+            _, _, _, encodings = vq_vae(batch)
+            original = encodings.reshape(-1, image_size, image_size)
+
+        # Run our model & get outputs
+        reconstructed, mu, log_var, _ = model(original)
+
+        # Calculate loss
+        reconstruction_loss = criterion(reconstructed, original)
+        kl_d_loss = loss.kl_divergence(mu, log_var, use_sum=False)
+        final_loss = reconstruction_loss + (kl_d_weight * kl_d_loss)
+
+        # Backprop
+        final_loss.backward()
+
+        # Update our optimizer parameters
+        optimizer.step()
+
+        # Add the batch's loss to the total loss for the epoch
+        train_loss += final_loss.item()
+        train_recon_loss += reconstruction_loss.item()
+        train_kl_d += kl_d_loss.item()
+
+    # Validation Loop
+    model.eval()
+    with torch.no_grad():
+        for iteration, batch in enumerate(tqdm(val_dataloader)):
+            # Move batch to device
+            _, batch = batch  # Returns key, value for each Pokemon
+            batch = batch.to(device)
+
+            # Get Encodings from vq_vae
+            _, _, _, encodings = vq_vae(batch)
+            original = encodings.reshape(-1, image_size, image_size)
+
+            # Run our model & get outputs
+            reconstructed, mu, log_var, _ = model(original)
+
+            # Calculate loss
+            reconstruction_loss = criterion(reconstructed, original)
+            kl_d_loss = loss.kl_divergence(mu, log_var, use_sum=False)
+            final_loss = reconstruction_loss + (kl_d_weight * kl_d_loss)
+
+            # Add the batch's loss to the total loss for the epoch
+            val_loss += final_loss.item()
+            val_recon_loss += reconstruction_loss.item()
+            val_kl_d += kl_d_loss.item()
+
+        # Get reconstruction of our sample
+        target_shape = (sample.shape[0], image_size, image_size, vq_vae_embedding_dim)
+        _, _, _, encodings = vq_vae(sample.to(device))
+        epoch_sample = encodings.reshape(-1, image_size, image_size)
+        epoch_sample, _, _, _ = model(epoch_sample)
+        epoch_sample = epoch_sample.argmax(dim=1, keepdim=True)
+        epoch_sample = epoch_sample.flatten(start_dim=1).view(-1, 1)
+        epoch_sample = vq_vae.quantize_and_decode(epoch_sample, target_shape, device)
+
+    # Add sample reconstruction to our list
+    all_samples.append(epoch_sample.detach().cpu())
+
+    # Compute the average losses for this epoch
+    train_loss = train_loss / len(train_dataloader)
+    train_recon_loss = train_recon_loss / len(train_dataloader)
+    train_kl_d = train_kl_d / len(train_dataloader)
+    all_train_loss.append((train_loss, train_recon_loss, train_kl_d))
+
+    val_loss = val_loss / len(val_dataloader)
+    val_recon_loss = val_recon_loss / len(val_dataloader)
+    val_kl_d = val_kl_d / len(val_dataloader)
+    all_val_loss.append((val_loss, val_recon_loss, val_kl_d))
+
+    # Print Metrics
+    print(
+        f"\nEpoch: {epoch+1}/{epochs}:\
+        \nTrain Loss = {train_loss}\
+        \nTrain Reconstruction Loss = {train_recon_loss}\
+        \nTrain KL Divergence = {train_kl_d}\
+        \nVal Loss = {val_loss}\
+        \nVal Reconstruction Loss = {val_recon_loss}\
+        \nVal KL Divergence = {val_kl_d}"
+    )
+
+################################################################################
+################################## Save & Test #################################
+################################################################################
+# Generate Loss Graph
+graphics.draw_loss(all_train_loss, all_val_loss, loss_output_path, mode="vae")
+
+# Save Model
+torch.save(model.state_dict(), model_output_path)
+
+# Plot Animation Sample
+fig, axis = graphics.make_grid(("Sample", sample), 4, 4)
+plt.savefig(animation_sample_image_name)
+
+# Create & Save Animation
+anim = graphics.make_animation(graphics.make_grid, all_samples)
+anim.save(animation_output_path)
+
+model.eval()
+
+# Evaluate on Test Images
+# Save Generated Images & Calculate Metrics
+# Testing Loop - Standard
+test_loss = 0
+test_reconstruction = 0
+test_kl_d = 0
+with torch.no_grad():
+    for iteration, batch in enumerate(tqdm(test_dataloader)):
+        # Move batch to device
+        filenames, batch = batch  # Returns key, value for each Pokemon
+        batch = batch.to(device)
+
+        # Get Encodings from vq_vae
+        _, _, _, encodings = vq_vae(batch)
+        original = encodings.reshape(-1, image_size, image_size)
+
+        # Run our model & get outputs
+        reconstructed, mu, log_var, _ = model(original)
+
+        # Calculate loss
+        reconstruction_loss = criterion(reconstructed, original)
+        kl_d_loss = loss.kl_divergence(mu, log_var, use_sum=False)
+        final_loss = reconstruction_loss + (kl_d_weight * kl_d_loss)
+
+        # Add the batch's loss to the total loss for the epoch
+        test_reconstruction += reconstruction_loss.item()
+        test_kl_d += kl_d_loss.item()
+        test_loss += final_loss.item()
+
+        # Save
+        target_shape = (
+            reconstructed.shape[0],
+            image_size,
+            image_size,
+            vq_vae_embedding_dim,
+        )
+        reconstructed = reconstructed.argmax(dim=1, keepdim=True)
+        reconstructed = reconstructed.flatten(start_dim=1).view(-1, 1)
+        reconstructed = vq_vae.quantize_and_decode(reconstructed, target_shape, device)
+        reconstructed = reconstructed.permute(0, 2, 3, 1).detach().cpu().numpy()
+        for image, filename in zip(reconstructed, filenames):
+            plt.imsave(os.path.join(output_dir, filename), image)
+
+# Print Metrics
+test_loss = test_loss / len(test_dataloader)
+test_reconstruction = test_reconstruction / len(test_dataloader)
+test_kl_d = test_kl_d / len(test_dataloader)
+print(
+    f"\nTest Loss:\
+        \nTest Loss = {test_loss}\
+        \nTest Reconstruction Loss = {test_reconstruction}\
+        \nTest KL Divergence = {test_kl_d}"
+)
+
+# Pick a couple of sample images for an Input v Output comparison
+test_sample = data.get_samples_from_data(test_data, 16)
+
+# Plot A Set of Test Images
+fig, axis = graphics.make_grid(("Test Sample", test_sample), 4, 4)
+plt.savefig(test_sample_input_name)
+
+target_shape = (test_sample.shape[0], image_size, image_size, vq_vae_embedding_dim)
+with torch.no_grad():
+    _, _, _, encodings = vq_vae(test_sample.to(device))
+    test_sample = encodings.reshape(-1, image_size, image_size)
+    test_sample, _, _, _ = model(test_sample)
+    test_sample = test_sample.argmax(dim=1, keepdim=True)
+    test_sample = test_sample.flatten(start_dim=1).view(-1, 1)
+    test_sample = vq_vae.quantize_and_decode(test_sample, target_shape, device)
+    reconstructed = test_sample.detach().cpu()
+
+# Plot A Set of Reconstructed Test Images
+fig, axis = graphics.make_grid(("Test Sample", reconstructed), 4, 4)
+plt.savefig(test_sample_output_name)
